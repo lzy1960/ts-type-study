7399441f710481c0ddc7cca664d127baabb47154
lzy1960 <1960553899@qq.com>
2022-06-13 23:32:37 +0800

    Type Lookup

---
+++ type-challenges/62-LookUp/template.ts
@@ -0,0 +1,7 @@
+// 有时，您可能希望根据其属性在并集中查找类型。
+// 在此挑战中，我们想通过在联合Cat | Dog中搜索公共type字段来获取相应的类型。换句话说，在以下示例中，我们期望LookUp<Dog | Cat, 'dog'>获得Dog，LookUp<Dog | Cat, 'cat'>获得Cat。
+
+type LookUp<U, T> = U extends { type: T } ? U : never
+
+// 第一次没注意有公共字段type
+// 以为是遍历每个字段找
