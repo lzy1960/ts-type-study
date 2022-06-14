a25173315399ea2655a35e8caa8109bf009fecc7
lzy1960 <1960553899@qq.com>
2022-06-14 18:33:59 +0800

    Absolute

---
+++ type-challenges/529-Absolute/template.ts
@@ -0,0 +1,11 @@
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer P}`
+  ? P
+  : `${T}`
+
+// 第一次答案，number的用例都没通过
+// type Absolute<T extends number | string | bigint> =
+//   T extends `${infer First}${infer Rest}` ? (First extends '-' ? Rest : T) : any
+
+// 应该先把泛型转换为字符串，再去判断，删掉负号
+// 最后一行，bigInt会自动删除中间的下划线
+// 但普通的number不会
