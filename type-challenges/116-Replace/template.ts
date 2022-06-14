4eb9f6d3bb3092d91f9d67af6df7151a391b23dc
lzy1960 <1960553899@qq.com>
2022-06-14 13:40:55 +0800

    Replace

---
+++ type-challenges/116-Replace/template.ts
@@ -0,0 +1,27 @@
+// 实现 Replace<S, From, To> 将字符串 S 中的第一个子字符串 From 替换为 To
+
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ''
+  ? S
+  : S extends `${infer Left}${From}${infer Right}`
+  ? `${Left}${To}${Right}`
+  : S
+
+// 1.
+// type Replace<
+//   S extends string,
+//   From extends string,
+//   To extends string
+// > = S extends `${From}${infer Rest}` | `${infer Rest}${From}`
+//   ? `${To}${Rest}`
+//   : S
+
+// 上面这种方法，会有一个测试用例过不去
+
+// 2.
+// S extends `${infer Left}${From}${infer Right}` ? `${Left}${To}${Right}` : S
+
+//上面这个方法，也是有一个过不去，需要排除From为空字符串的情况
