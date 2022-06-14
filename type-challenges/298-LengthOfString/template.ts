3944eaeccdfcaa96c5343edcfda5644a4c639cea
lzy1960 <1960553899@qq.com>
2022-06-14 16:54:28 +0800

    Length Of String

---
+++ type-challenges/298-LengthOfString/template.ts
@@ -0,0 +1,14 @@
+// 计算字符串的长度，类似于 String#length
+
+type LengthOfString<
+  S extends string,
+  U extends any[] = []
+> = S extends `${infer First}${infer Rest}`
+  ? LengthOfString<Rest, [...U, First]>
+  : U['length']
+
+// 字符串没有长度，要转换成元组
+// 1. S中取第一个
+// 2. 放到数组最后
+// 3. 直到S为空
+// 4. 最后获取元组的长度
