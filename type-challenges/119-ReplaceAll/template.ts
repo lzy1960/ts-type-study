94ad89e02610a587fefcf72869362e966eb7d157
lzy1960 <1960553899@qq.com>
2022-06-14 13:54:19 +0800

    ReplaceAll

---
+++ type-challenges/119-ReplaceAll/template.ts
@@ -0,0 +1,13 @@
+// 实现 ReplaceAll<S, From, To> 将一个字符串 S 中的所有子字符串 From 替换为 To
+
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ''
+  ? S
+  : S extends `${infer Left}${From}${infer Right}`
+  ? `${Left}${To}${ReplaceAll<Right, From, To>}`
+  : S
+
+// 用Replace+递归实现
