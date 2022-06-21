de1c4b3f6c48162e1a10b15c35ab26e5cf9aa6c1
lzy1960 <1960553899@qq.com>
2022-06-21 17:05:28 +0800

    PickByType

---
+++ type-challenges/2595-PickByType/template.ts
@@ -0,0 +1,7 @@
+// From T, pick a set of properties whose type are assignable to U.
+
+type PickByType<T, U> = {
+  [P in keyof T as U extends T[P] ? P : never]: T[P]
+}
+
+// 要检测key的类型，需要在中括号里做断言+判断
