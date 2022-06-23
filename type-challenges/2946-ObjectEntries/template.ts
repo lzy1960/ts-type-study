77d99d0a81fadea2d52f2347ee529eea9589b3fa
lzy1960 <1960553899@qq.com>
2022-06-23 15:05:40 +0800

    ObjectEntries

---
+++ type-challenges/2946-ObjectEntries/template.ts
@@ -0,0 +1,11 @@
+// Implement the type version of Object.entries
+
+type ObjectEntries<T> = {
+  [P in keyof T]-?: [P, T[P] extends infer R | undefined ? R : T[P]]
+}[keyof T]
+
+// 应该是要用递归，每次都把处理完的 key 排除
+// 但是看了答案，并不需要递归
+
+// 这个方法是：
+// 把每一项都转化 key: [key, value] 的格式，然后根据 key 取出全部的value
