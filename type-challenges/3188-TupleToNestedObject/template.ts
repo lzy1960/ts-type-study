7e12de2cacdfa9395916bccab26ccf608f78816f
lzy1960 <1960553899@qq.com>
2022-06-23 15:37:05 +0800

    fix: TupleToNestedObject key类型报错

--- type-challenges/3188-TupleToNestedObject/template.ts
+++ type-challenges/3188-TupleToNestedObject/template.ts
@@ -1,9 +1,9 @@
 // Given a tuple type T that only contains string type, and a type U, build an object recursively.
 
 type TupleToNestedObject<T, U> = T extends [infer F, ...infer Rest]
-  ? { [K in F]: TupleToNestedObject<Rest, U> }
+  ? { [K in F & string]: TupleToNestedObject<Rest, U> }
   : U
 
 // F 报错
 // 不能将类型“F”分配给类型“string | number | symbol”。ts(2322)
-// TODO:目前还不知道怎么解决
+// F 指定为 string 就能解决
