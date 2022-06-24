a3541739e0a2d575af38f30364c0807fe845f2ab
lzy1960 <1960553899@qq.com>
2022-06-24 20:53:52 +0800

    InorderTraversal

---
+++ type-challenges/3376-InorderTraversal/template.ts
@@ -0,0 +1,10 @@
+// Implement the type version of binary tree inorder traversal.
+
+interface TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
+  ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>]
+  : []
