0f65887decab27b4e2dd9676ecd98b3d42234c60
lzy1960 <1960553899@qq.com>
2022-07-03 17:05:39 +0800

    IsTuple

---
+++ type-challenges/4471-IsTuple/template.ts
@@ -0,0 +1,14 @@
+// Implement a type IsTuple, which takes an input type T and returns whether T is tuple type.
+
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly unknown[]
+  ? number extends T['length']
+    ? false
+    : true
+  : false
+
+// 如何检测是否是元组？
+// 根据长度来检测
+// number extends T['length'] 解释如下：
+// 数组的长度是number类型，元组的长度是一个具体的number
